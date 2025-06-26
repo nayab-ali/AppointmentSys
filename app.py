aa3e443f4df9559460398d34ea9af99b7ca9f0d0
Nayab Ali <nayabmca2024@gmail.com>
2025-06-26 12:33:39 +0530

    Use dynamic port for Railway deployement

--- app.py
+++ app.py
@@ -946,4 +946,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True, port=int(os.environ.get('PORT', 8080)))
